af537f313f5fbd45867f36065e9ffccfddd43699
Xandi <santosal2018@gmail.com>
2023-08-08 12:40:21 -0400

    add auto increment to id's in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE my_company;
 USE my_company;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
